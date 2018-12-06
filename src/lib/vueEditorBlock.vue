ecfd5e31f47d9545d390c105c96b38563196820e
起床啦！ <1106728884@qq.com>
2018-12-06 17:20:58 +0800

    样式扩展添加

--- src/lib/vueEditorBlock.vue
+++ src/lib/vueEditorBlock.vue
@@ -4,10 +4,10 @@
 */
 /*
 * @LastEditors: aFei
-* @LastEditTime: 2018-12-03 16:00:33
+* @LastEditTime: 2018-12-06 17:19:01
 */
 <template>
-    <div>
+    <div class="vueEditorBlock extra">
         1
     </div>
 </template>
@@ -18,6 +18,13 @@
     }
 </script>
 
+<!--基础样式-->
 <style scoped>
+  @import "vueEditorBlock.css";
+</style>
 
+<!--样式扩展-->
+<style scoped>
+  /*必需的扩展*/
+  @import "editorExtra.css";
 </style>
