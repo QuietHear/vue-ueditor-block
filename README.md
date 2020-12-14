2836a35c3dd06ec04a534a6d0c2254c063ea0471
yps <afei@email.com>
2020-12-14 14:26:05 +0800

    组件功能更新

--- README.md
+++ README.md
@@ -6,10 +6,51 @@
 ### 0. 依赖组件
 * 安装依赖 `npm i vue-ueditor-wrap`
 
-### 1. 参数
-* `httpParams`：请求附带参数-->Object;非必传
-* `urlHost`：自定义class-->String;非必传
-* `content`：自定义class-->String;非必传
-* `onlyShow`：只展示内容-->Boolean;非必传;默认false
+### 1. 下载静态资源到本地
+在本node包中下载static中的资源，前端的为UEditor文件夹，后端根据类型自己选择需要版本的.zip文件
 
-### 2. 示例数据
+### 2. 参数
+* `v-model`：输入区对应的内容，双向绑定-->String;非必传
+* `staticUrl`：静态资源文件地址,指向index.html-->String;非必传;默认'/static/UEditor/'
+* `cname`：自定义class-->String;非必传
+* `uploadUrl`：图片上传的接口,后端可以看[文档](http://fex.baidu.com/ueditor/#server-deploy '后端文档')-->String;非必传
+* `httpParams`：请求附带参数-->Object;非必传
+* `onlyShow`：只展示内容-->Boolean;非必传;默认false
+* `extraConfig`：ueditor配置项,如与预设相同,则会覆盖之前的-->Object;非必传
+* `@change`：内容改变监听-->返回当前内容
+
+## 3. 方法
+* `getExample`：获取当前ueditor的实例，可以使用其API
+
+### 4. 组件预设ueditor配置
+	// 编辑器不自动被内容撑高
+    autoHeightEnabled: false,
+    // 定制菜单
+    toolbars: [
+      [
+        "fullscreen",
+        "bold",
+        "italic",
+        "underline",
+        "fontsize",
+        "insertimage",
+        "insertorderedlist",
+        "insertunorderedlist",
+      ],
+      ["source"], // 调试时候使用
+    ],
+    // 初始容器高度
+    initialFrameHeight: 240,
+    // 初始容器宽度
+    initialFrameWidth: "100%",
+    // 关闭自动保存
+    enableAutoSave: false,
+    // 是否启用元素路径
+    elementPathEnabled: false,
+    // 内容只读
+    readonly: false,
+    // 开启字数统计
+    wordCount: true,
+
+### 5. 官方文档
+[http://fex.baidu.com/ueditor/#start-start](http://fex.baidu.com/ueditor/#start-start '文档')
--- lib/vueEditorBlock.vue
+++ lib/vueEditorBlock.vue
@@ -1,30 +1,184 @@
 /*
 * @Author: aFei
-* @Date: 2018-12-03 16:00:30
+* @Date: 2018-11-26 10:51:05
 */
 /*
-* @LastEditors: aFei
-* @LastEditTime: 2018-12-06 17:19:01
+ * @LastEditors: afei
+ * @LastEditTime: 2020-12-14 14:01:35
 */
 <template>
-    <div class="vue-editor-block extra">
-        1
-    </div>
+  <vue-ueditor-wrap
+    :class="['vue-editor-block', onlyShow ? 'only-show' : '', cname]"
+    v-model="msg"
+    :config="config"
+    @ready="ready"
+    :init="myInit"
+  ></vue-ueditor-wrap>
 </template>
 
 <script>
-    export default {
-        name: "vueEditorBlock"
+// 1、引入VueUeditorWrap组件
+import VueUeditorWrap from "vue-ueditor-wrap";
+
+export default {
+  name: "vueEditorBlock",
+  // 2、注册组件
+  components: {
+    VueUeditorWrap,
+  },
+  model: {
+    prop: "content",
+    event: "changeValue",
+  },
+  props: {
+    content: {
+      type: String,
+      default: "",
+    },
+    staticUrl: {
+      // 静态资源文件地址
+      type: String,
+      default: "/static/UEditor/",
+    },
+    cname: {
+      // 额外class
+      type: String,
+      default: "",
+    },
+    uploadUrl: {
+      // 图片上传的接口
+      type: String,
+      default: "",
+    },
+    httpParams: {
+      // 请求附带参数
+      type: Object,
+      default: function () {
+        return {
+          none: true,
+        };
+      },
+    },
+    onlyShow: {
+      // 只展示内容，不展示菜单
+      type: Boolean,
+      default: false,
+    },
+    extraConfig: {
+      // ueditor配置项
+      type: Object,
+      default: function () {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      // 3、v-model绑定数据
+      msg: "",
+      // 4、根据项目需求自行配置,具体配置参见ueditor.config.js源码或 http://fex.baidu.com/ueditor/#start-start
+      config: {
+        // 如果需要上传功能,找后端小伙伴要服务器接口地址
+        serverUrl: this.uploadUrl,
+        // 你的UEditor资源存放的路径,相对于打包后的index.html
+        UEDITOR_HOME_URL: this.staticUrl,
+        // 编辑器不自动被内容撑高
+        autoHeightEnabled: false,
+        // 定制菜单
+        toolbars: [
+          [
+            "fullscreen",
+            "bold",
+            "italic",
+            "underline",
+            "fontsize",
+            "insertimage",
+            "insertorderedlist",
+            "insertunorderedlist",
+          ],
+          ["source"], // 调试时候使用
+        ],
+        // 初始容器高度
+        initialFrameHeight: 240,
+        // 初始容器宽度
+        initialFrameWidth: "100%",
+        // 关闭自动保存
+        enableAutoSave: false,
+        // 是否启用元素路径
+        elementPathEnabled: false,
+        // 内容只读
+        readonly: false,
+        // 开启字数统计
+        wordCount: true,
+      },
+      example: "",
+    };
+  },
+  methods: {
+    // 5、 你可以在ready方法中拿到editorInstance实例,之后的所有API就和官方的实例一样了,Just Do What You Want! http://fex.baidu.com/ueditor/#api-common
+    ready(ue) {
+      this.example = ue;
+      if (!this.httpParams.none) {
+        ue.execCommand("serverparam", this.httpParams);
+      }
+      ue.addListener("contentChange", this.msgChange);
+      ue.addListener("catchRemoteImage", this.msgChange);
+    },
+    msgChange() {
+      this.$emit("changeValue", this.msg);
+      this.$emit("change", this.msg);
+    },
+    // 6. 结合init方法,自定义按钮
+    myInit() {
+      // this.$refs.ueditor.forEach((vm) => {
+      //   vm.registerButton({
+      //     name: 'test',
+      //     icon: './public/test-button.png',
+      //     tip: 'this is a test tip',
+      //     handler: (editor, name) => {
+      //       editor.execCommand('inserthtml', `<span>text inserted by test button</span>`)
+      //     }
+      //   })
+      //   vm.registerButton({
+      //     name: 'center',
+      //     icon: './public/center.png',
+      //     tip: '表格居中',
+      //     handler: (editor, name) => {
+      //       var tables = editor.document.querySelectorAll('table')
+      //       if (tables.length) {
+      //         tables.forEach((table) => {
+      //           table.style.margin = '0 auto'
+      //         })
+      //       } else {
+      //         editor.trigger('showmessage', {
+      //           content: '没有表格',
+      //           timeout: 2000
+      //         })
+      //       }
+      //     }
+      //   })
+      // })
+    },
+    getExample() {
+      return this.example;
+    },
+  },
+  created() {
+    if (this.onlyShow) {
+      this.config.autoHeightEnabled = true;
+      this.config.toolbars = [];
+      this.config.wordCount = false;
+      this.config.readonly = true;
     }
+    for (let key in this.extraConfig) {
+      this.config[key] = this.extraConfig[key];
+    }
+    this.msg = this.content;
+  },
+};
 </script>
 
 <!--基础样式-->
 <style scoped>
-  @import "style/vueEditorBlock.css";
-</style>
-
-<!--样式扩展-->
-<style scoped>
-  /*必需的扩展*/
-  @import "style/editorExtra.css";
+@import "style/vueEditorBlock.css";
 </style>
